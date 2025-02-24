e802f82a7468aeafa0553201575dcffbbd183a8c
aps50 <414catherine@gmail.com>
2025-02-24 22:10:46 +0900

    [feat] Updated and changed layers to use

--- include/layer.h
+++ include/layer.h
@@ -16,7 +16,16 @@ void Add(Tensor *in1, Tensor *in2, Tensor *in3, Tensor *in4,
          Tensor *out);
 
 /* Example of using CUDA kernel */
+void Conv1D_CUDA(Tensor *in, Tensor *w, Tensor *b, Tensor *out);
 void ReLU_CUDA(Tensor *inout);
+void GetMax_CUDA(Tensor *in, Tensor *out);
+void Concat_CUDA(Tensor *in1, Tensor *in2, Tensor *in3, Tensor *in4, 
+            Tensor *out);
+void Linear_CUDA(Tensor *in, Tensor *w, Tensor *b, Tensor *out);
+void Softmax_CUDA(Tensor *inout);
+void Scaling_CUDA(Tensor *inout, float s);
+void Add_CUDA(Tensor *in1, Tensor *in2, Tensor *in3, Tensor *in4, 
+         Tensor *out);
 
 /* [Advanced] Example of using half-precision on CPU */
 void Linear_Half(Tensor *in, Tensor *w, Tensor *b, Tensor *out);    
