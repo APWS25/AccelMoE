dc17b58bed24df4f04bf89fed3310da5121097ea
aps50 <414catherine@gmail.com>
2025-02-25 21:24:46 +0900

    [feat] Changed Tensor implement

--- src/tensor.cu
+++ src/tensor.cu
@@ -15,9 +15,11 @@ Tensor::Tensor(const vector<size_t> &shape_) {
   ndim = shape_.size();
   for (size_t i = 0; i < ndim; i++) { shape[i] = shape_[i]; }
   size_t N_ = num_elem();
-  //CHECK_CUDA(cudaMalloc((void**)&buf, sizeof(float) * N_)); // paged - Segmentation fault (core dumped)
+  // CPU
   CHECK_CUDA(cudaMallocHost((void**)&buf, sizeof(float) * N_)); // pinned
-  CHECK_CUDA(cudaMemset(buf, 0, sizeof(float) * N_));
+  // GPU
+  CHECK_CUDA(cudaMalloc((void**)&gbuf, sizeof(float) * N_)); 
+  CHECK_CUDA(cudaMemset(gbuf, 0, sizeof(float) * N_));
 }
 
 // 생성자: 기존 버퍼 사용
@@ -25,15 +27,43 @@ Tensor::Tensor(const vector<size_t> &shape_, float *buf_) {
   ndim = shape_.size();
   for (size_t i = 0; i < ndim; i++) { shape[i] = shape_[i]; }
   size_t N_ = num_elem();
-  //CHECK_CUDA(cudaMalloc((void**)&buf, sizeof(float) * N_)); // paged - Segmentation fault (core dumped)
+  // CPU
   CHECK_CUDA(cudaMallocHost((void**)&buf, sizeof(float) * N_)); // pinned
-  CHECK_CUDA(cudaMemcpy(buf, buf_, N_ * sizeof(float), cudaMemcpyHostToDevice));
+  memcpy(buf, buf_, N_ * sizeof(float));
+  // GPU
+  CHECK_CUDA(cudaMalloc((void**)&gbuf, sizeof(float) * N_)); 
+  CHECK_CUDA(cudaMemcpy(gbuf, buf_, N_ * sizeof(float), cudaMemcpyHostToDevice));
+}
+
+void Tensor::compare() {
+  size_t N_ = num_elem();
+  float* tmp = new float[N_];
+  CHECK_CUDA(cudaMemcpy(tmp, gbuf, N_ * sizeof(float), cudaMemcpyDeviceToHost));
+  
+  for (size_t i = 0; i < N_; i++) {
+      if (fabs(buf[i] - tmp[i]) > 1e-5) {
+          printf("Mismatch at index %zu: CPU: %f GPU: %f\n", i, buf[i], tmp[i]);
+      }
+  }
+  delete[] tmp;
+}
+
+// GPU에서 CPU로 데이터 복사
+void Tensor::toCPU() {
+  size_t N_ = num_elem();
+  CHECK_CUDA(cudaMemcpy(buf, gbuf, N_ * sizeof(float), cudaMemcpyDeviceToHost));
+}
+
+// CPU에서 GPU로 데이터 복사
+void Tensor::toGPU() {
+  size_t N_ = num_elem();
+  CHECK_CUDA(cudaMemcpy(gbuf, buf, N_ * sizeof(float), cudaMemcpyHostToDevice));
 }
 
 // Tensor 소멸자
 Tensor::~Tensor() {
-  //if (buf != nullptr) CHECK_CUDA(cudaFree(buf)); // paged - Segmentation fault (core dumped)
   if (buf != nullptr) CHECK_CUDA(cudaFreeHost(buf));
+  if (gbuf != nullptr) CHECK_CUDA(cudaFree(gbuf));
 }
 
 // 텐서의 총 원소 개수를 반환하는 함수
